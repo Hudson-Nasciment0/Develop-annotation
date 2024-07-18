4d418c3b6f22d5f3c794667cb1cfc1a7881dc72f
Hudson-Nasciment0 <nascimentoh714@gmail.com>
2024-07-17 21:56:19 -0300

    anotações do for

---
+++ For.js
@@ -0,0 +1,18 @@
+/*Estrutura de Repetição - L00P
+
+--FOR
+
+    1) INICIALIZAÇÃO - criar uma variavel e colocar um valor inicial para ela;
+    2) CONDIÇÃO - Enquanto for true, o laço continuará iterando.
+        Ele irá verificar antes de cada iteração
+    3) EXPRESSÃO FINAL - O que irá ocorrer a cada vez que nosso
+    
+    for ([inicialização]; [condição]; [expressção final]){
+        Seu código aqui
+        }
+*/
+
+for(i = 0; i < 15; i++ ){
+    console.log(i)
+}
+
