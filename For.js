742df37e41625a5c73eca6e92917f521e2294486
Hudson-Nasciment0 <nascimentoh714@gmail.com>
2024-07-28 21:16:28 -0300

    for + desafio for

--- For.js
+++ For.js
@@ -5,14 +5,19 @@
     1) INICIALIZAÇÃO - criar uma variavel e colocar um valor inicial para ela;
     2) CONDIÇÃO - Enquanto for true, o laço continuará iterando.
         Ele irá verificar antes de cada iteração
-    3) EXPRESSÃO FINAL - O que irá ocorrer a cada vez que nosso
-    
-    for ([inicialização]; [condição]; [expressção final]){
+    3) EXPRESSÃO FINAL - O que irá ocorrer a cada vez que nosso for ([inicialização]; [condição]; [expressção final]){
         Seu código aqui
         }
 */
-
-for(i = 0; i < 15; i++ ){
-    console.log(i)
+// length(tamanho do array) = 3
+const users = [ 
+    "Maria",
+    "Aline",
+    "João",
+    
+]
+// users.length => Tamanho do meu array
+for (let i = 0; i < users.length; i++){
+    console.log(users[i])
 }
 
