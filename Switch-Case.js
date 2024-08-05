4c4e72ef1d3868d39c173e0ed277c5b963d1cef0
Hudson-Nasciment0 <nascimentoh714@gmail.com>
2024-08-05 19:56:39 -0300

    setTimeout e switch-case

---
+++ Switch-Case.js
@@ -0,0 +1,26 @@
+/*
+    Switch Case - Controlador de Fluxo
+*/
+
+const user = "Maria"
+
+ 
+
+switch (user) {
+    case "Rodolfo":
+        console.log("Ta meio frio")
+        break;
+
+        case "Maria":
+        console.log("Ta meio quente")
+        break;
+
+        case "Joaquim":
+        console.log("Ta quente")
+        break;
+
+
+    default: 
+        console.log("Não sei te dizer")
+        break;
+}
