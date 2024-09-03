4fdf7b03fed03679f923291ca02c5e107cabe5f4
Hudson-Nasciment0 <nascimentoh714@gmail.com>
2024-09-02 21:24:36 -0300

    function return

---
+++ Function-Return.js
@@ -0,0 +1,19 @@
+
+
+
+
+/*
+    Calculo Desconto: 
+
+Todos os Produtos Acima de R$ 30,00 tem desconto de 10%
+
+*/
+
+
+const cart = [10 , 244, 99, 2, 20, 33, 250]
+
+function descont (value, value2){
+
+}
+
+descont()
